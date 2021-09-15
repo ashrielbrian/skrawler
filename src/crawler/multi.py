ca2ad1a7e2961b37e9f05e707d3094e8bba390ba
Brian Tang <ashrielbrian@gmail.com>
2021-09-15 17:32:33 +0800

    Updates return obj types when getting obj keys to crawl

--- src/crawler/multi.py
+++ src/crawler/multi.py
@@ -1,19 +1,15 @@
 import ray
 import boto3
 from core import stream
+from typing import List
 
-# 1. get files from bucket to process -- get dtypes
-# 2. create tasks and assign files
-# 3. execute and store returned metadata
-# 4. store metadata to file on callback
-
-def get_files_from_s3(bucket: str, file_prefix: str):
+def get_files_from_s3(bucket: str, file_prefix: str) -> List[boto3.resources.factory.s3.ObjectSummary]:
     """ Gets a list of object keys from S3 """
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(bucket)
 
     s3_objs = bucket.objects.filter(Prefix=file_prefix).limit(10)
-    return [obj.key for obj in s3_objs]
+    return [obj for obj in s3_objs]
 
 @ray.remote
 def async_s3_file_processing(**kwargs):
